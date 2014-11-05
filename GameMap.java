1d6ab16722243f152205ffd9717626eec48b1565
Aaron <palkiax50@topmail.ie>
2014-11-05 15:58:00 +0000

    added height and width to GameMap

--- GameMap.java
+++ GameMap.java
@@ -1,20 +1,46 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public abstract class GameMap {
 	String detailOne;
 	String detailTwo;
-	ArrayList areas = new ArrayList();
+	List areas;
+	int width, height;
+	
+	public GameMap() {
+		areas = new ArrayList();
+	}
+	
 	void addHomeGround() {
 		//do stuff
 	}
+	
 	void addNotTraversibleTerrain() {
 		//do stuff
 	}
+	
 	void addSurroundingTerrain() {
 		//do stuff
 	}
+	
 	public String toString() {
 		//Add some stuff
 		return "";
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public void setWidth(int val) {
+		width = val;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public void setHeight(int val) {
+		height = val;
 	}
 }
